15e1d70571135d1e75210ef1e02f94d041f87eb5
Jinta <Jinta@163.con>
2013-05-21 23:49:40 +0800

    add some code of VideoPlayerActivity.java

--- MultiMediaSystem/src/com/macya/multimedia/vedio/VideoPlayerActivity.java
+++ MultiMediaSystem/src/com/macya/multimedia/vedio/VideoPlayerActivity.java
@@ -1,7 +1,11 @@
 package com.macya.multimedia.vedio;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class VideoPlayerActivity extends Activity{
-
+	@Override 
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
 }
